898ee634e60f569b2a2a62eceb58539ea73c35f1
DivMak11 <div.makar@gmail.com>
2026-01-08 06:01:50 +0530

    IOS support done: Config Left

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,8 @@
 import Flutter
 import UIKit
+import FirebaseCore
+import FirebaseMessaging
+import UserNotifications
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +10,43 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Configure Firebase
+    FirebaseApp.configure()
+    
+    // Set up UNUserNotificationCenter delegate for foreground notifications
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self
+    }
+    
+    // Register for remote notifications
+    application.registerForRemoteNotifications()
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+  
+  // Handle foreground notification presentation - show alert even when app is in foreground
+  override func userNotificationCenter(
+    _ center: UNUserNotificationCenter,
+    willPresent notification: UNNotification,
+    withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+  ) {
+    // Show notification banner, play sound, and update badge even when app is in foreground
+    if #available(iOS 14.0, *) {
+      completionHandler([[.banner, .sound, .badge]])
+    } else {
+      completionHandler([[.alert, .sound, .badge]])
+    }
+  }
+  
+  // Handle notification tap - just opens the app (no deep linking per user requirement)
+  override func userNotificationCenter(
+    _ center: UNUserNotificationCenter,
+    didReceive response: UNNotificationResponse,
+    withCompletionHandler completionHandler: @escaping () -> Void
+  ) {
+    // Let Flutter plugin handle the notification tap
+    // This will simply open the app to the last visible screen
+    completionHandler()
   }
 }
